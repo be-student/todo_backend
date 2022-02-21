36e81e8c218c3409c3e00dfa3e3c97e1f0ee3363
be-student <presentsong@naver.com>
2022-02-21 15:10:01 +0900

    remove verify token

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -14,13 +14,3 @@ export const isLoggedIn = (req, res, next) => {
     return res.json({ result: "failure", error: "not loggedIn" });
   }
 };
-export const verifyToken = (req, res, next) => {
-  try {
-    console.log(req.headers);
-    //req.decoded = Jwt.verify(req.headers.authorization, JWT_SECRET);
-    res.json({ result: "asdf" });
-  } catch (error) {
-    console.error(error);
-    return res.json({ result: "failure", error: "token has error" });
-  }
-};
