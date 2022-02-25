b19f8aee9ac91fe24e65ef2722155148bebc5ce9
be-student <presentsong@naver.com>
2022-02-25 22:30:01 +0900

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,12 @@ const app = express();
 //     methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
 //   })
 // );
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://13.125.209.129"],
+    credentials: true,
+  })
+);
 app.set("port", process.env.PORT || 8001);
 sequelize
   .sync({ force: false })
